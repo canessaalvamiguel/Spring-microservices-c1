cb4f96ce518d10e8ac848a93535e95c4bd74c0d7
Miguel Canessa <canessaalvamiguel@gmail.com>
2022-10-06 19:59:23 -0500

    Services - Enable integration with Eureka server

--- items-service/src/main/java/com/example/itemsservice/ItemsServiceApplication.java
+++ items-service/src/main/java/com/example/itemsservice/ItemsServiceApplication.java
@@ -2,12 +2,12 @@ package com.example.itemsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@RibbonClient(name = "products-service")
+@EnableEurekaClient
 public class ItemsServiceApplication {
 
 	public static void main(String[] args) {
