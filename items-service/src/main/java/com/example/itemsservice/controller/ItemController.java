2520792f7c4d68a15161a0cfbaab5efd19ba38f8
Miguel Canessa <canessaalvamiguel@gmail.com>
2022-10-14 11:37:47 -0500

    Item-Service - Circuit breaker and TimeLimiter using annotations

--- items-service/src/main/java/com/example/itemsservice/controller/ItemController.java
+++ items-service/src/main/java/com/example/itemsservice/controller/ItemController.java
@@ -3,6 +3,8 @@ package com.example.itemsservice.controller;
 import com.example.itemsservice.models.Item;
 import com.example.itemsservice.models.Product;
 import com.example.itemsservice.service.ItemService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -11,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 @RestController
@@ -35,7 +38,7 @@ public class ItemController {
         System.out.println("RequestHeader: "+ token);
         return ResponseEntity.ok(itemService.findAll());
     }
-    //@HystrixCommand(fallbackMethod = "alternativeMethod")
+
     @GetMapping("/items/{id}/amount/{amount}")
     public ResponseEntity<Item> details(@PathVariable("id") Long id, @PathVariable("amount") Integer amount) throws InterruptedException {
         return cbFactory.create("items")
@@ -43,6 +46,19 @@ public class ItemController {
                         () -> ResponseEntity.ok(itemService.findById(id, amount)),
                         e -> alternativeMethod(id, amount, e)
                 );
+    }
+
+    @GetMapping("/items2/{id}/amount/{amount}")
+    @CircuitBreaker(name = "items", fallbackMethod = "alternativeMethod")
+    public ResponseEntity<Item> details2(@PathVariable("id") Long id, @PathVariable("amount") Integer amount) throws InterruptedException {
+        return ResponseEntity.ok(itemService.findById(id, amount));
+    }
+
+    @GetMapping("/items3/{id}/amount/{amount}")
+    @TimeLimiter(name = "items", fallbackMethod = "alternativeMethod2")
+    @CircuitBreaker(name = "items")
+    public CompletableFuture<ResponseEntity<Item>> details3(@PathVariable("id") Long id, @PathVariable("amount") Integer amount) throws InterruptedException {
+        return CompletableFuture.supplyAsync( () ->  ResponseEntity.ok(itemService.findById(id, amount)) );
     }
 
     public ResponseEntity<Item> alternativeMethod(Long id, Integer amount, Throwable e){
@@ -56,5 +72,19 @@ public class ItemController {
 
         Item item = new Item(product, amount);
         return ResponseEntity.ok(item);
+    }
+
+    public CompletableFuture<ResponseEntity<Item>> alternativeMethod2(Long id, Integer amount, Throwable e){
+
+        logger.error("Error in details --> " + e.getMessage());
+
+        Product product = new Product();
+        product.setId(id);
+        product.setName("Camara Sony");
+        product.setPrice(5000.00);
+
+        Item item = new Item(product, amount);
+
+        return CompletableFuture.supplyAsync( () -> ResponseEntity.ok(item) );
     }
 }
