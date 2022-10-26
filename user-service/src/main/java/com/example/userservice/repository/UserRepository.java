fd9661e3bea5971127cc963a6d728ae7b387a288
Miguel Canessa <canessaalvamiguel@gmail.com>
2022-10-26 17:21:42 -0500

    User-Service - Create custom search method

--- user-service/src/main/java/com/example/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/example/userservice/repository/UserRepository.java
@@ -3,12 +3,15 @@ package com.example.userservice.repository;
 import com.example.userservice.models.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(path = "users")
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 
-    public User findByUsername(String username);
+    @RestResource(path = "find-username")
+    public User findByUsername(@Param("name") String username);
 
     @Query("select u from User u where u.username = ?1")
     public User getByUsername(String username);
