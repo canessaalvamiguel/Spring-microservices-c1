7377fc87390240c76f92cf200a5699331c8bc5b9
Miguel Canessa <canessaalvamiguel@gmail.com>
2022-10-28 12:03:57 -0500

    Gateway-Server - Configure Spring security

---
+++ gateway-server/src/main/java/com/example/gatewayserver/security/SpringSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.gatewayserver.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+public class SpringSecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain configure(ServerHttpSecurity http){
+        return http
+                .authorizeExchange()
+                .pathMatchers("/api/security/oauth/**")
+                .permitAll()
+                .pathMatchers(HttpMethod.GET, "/api/products/products", "/api/items/items", "/api/users/users")
+                .permitAll()
+                .pathMatchers(HttpMethod.GET,"/api/products/products/{id}", "/api/items/items/{id}", "/api/users/users/{id}")
+                .hasAnyRole("ADMIN", "USER")
+                .pathMatchers("/api/products/**", "/api/items/**", "/api/users/**")
+                .hasRole("ADMIN")
+                .anyExchange()
+                .authenticated()
+                .and()
+                .csrf()
+                .disable()
+                .build();
+    }
+}
